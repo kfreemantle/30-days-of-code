eaa999b7d15e8e7be0378c1bdf55c7d54aaa6934
Kyle Freemantle <117859478+kfreemantle@users.noreply.github.com>
2023-06-11 14:14:53 -0700

    Update Day01.js

--- Day01.js
+++ Day01.js
@@ -38,3 +38,23 @@ function main() {
 
 // Solution begins here
 
+    // Declare second integer, double, and String variables.
+let int
+let double
+let string
+   
+  // Read and save an integer, double, and String to your variables.
+  int = parseInt(readLine());
+  double = parseFloat(readLine());
+  string = readLine();
+
+  // Print the sum of both integer variables on a new line.
+  console.log(i + int);
+
+  // Print the sum of the double variables on a new line.
+  console.log((d + double).toFixed(1));
+
+  // Concatenate and print the String variables on a new line
+  // The 's' variable above should be printed first.
+  console.log(s + string)
+
